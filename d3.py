f096af6a4ea4768a1bec39afdd0a210aded4fa60
SGinovker <sginovke@edu.uwaterloo.ca>
2020-11-15 12:18:19 -0500

    data cleaning

--- d3.py
+++ d3.py
@@ -1,25 +1,75 @@
 import pandas as pd 
-from sklearn.model_selection import cross_validate
+import numpy as np
+from surprise.model_selection import cross_validate
 from math import sqrt
-
-#TODO: need to add col labels, formatting to csv (see d2 methodology) BEFORE running this (make separate script)
 
 #data combining+formatting
 
-df1 = pd.read_csv('combined_data1.txt',index_col=0)
-df2 = pd.read_csv('combined_data2.txt',index_col=0)
-df3 = pd.read_csv('combined_data3.txt',index_col=0)
-df4 = pd.read_csv('combined_data4.txt',index_col=0)
+df1 = pd.read_csv('combined_data1.txt',header = None, names = ['CustomerID', 'Rating'], usecols = [0,1])
+df1['Rating'] = df1['Rating'].astype(float)
+df2 = pd.read_csv('combined_data2.txt',header = None, names = ['CustomerID', 'Rating'], usecols = [0,1])
+df2['Rating'] = df2['Rating'].astype(float)
+df3 = pd.read_csv('combined_data3.txt',header = None, names = ['CustomerID', 'Rating'], usecols = [0,1])
+df3['Rating'] = df3['Rating'].astype(float)
+df4 = pd.read_csv('combined_data4.txt',header = None, names = ['CustomerID', 'Rating'], usecols = [0,1])
+df4['Rating'] = df4['Rating'].astype(float)
 
 frames = [df1,df2,df3,df4]
 #concat all data
 df = pd.concat(frames)
 
-#drop date col
-df.drop(columns='date')
+# add movieid col - courtesy of https://www.kaggle.com/laowingkin/netflix-movie-recommendation#Data-manipulation
+df_nan = pd.DataFrame(pd.isnull(df.Rating))
+df_nan = df_nan[df_nan['Rating'] == True]
+df_nan = df_nan.reset_index()
+
+movie_np = []
+movie_id = 1
+
+for i,j in zip(df_nan['index'][1:],df_nan['index'][:-1]):
+    # numpy approach
+    temp = np.full((1,i-j-1), movie_id)
+    movie_np = np.append(movie_np, temp)
+    movie_id += 1
+
+# Account for last record and corresponding length
+# numpy approach
+last_record = np.full((1,len(df) - df_nan.iloc[-1, 0] - 1),movie_id)
+movie_np = np.append(movie_np, last_record)
+
+# remove those Movie ID rows
+df = df[pd.notnull(df['Rating'])]
+
+df['MovieID'] = movie_np.astype(int)
+df['CustomerID'] = df['CustomerID'].astype(int)
+
+# courtesy of https://www.kaggle.com/laowingkin/netflix-movie-recommendation#Data-manipulation
+# get rid of data that is too sparse so it actually loads rip
+f=['count','mean']
+
+df_movie_summary = df.groupby('MovieID')['Rating'].agg(f)
+df_movie_summary.index = df_movie_summary.index.map(int)
+movie_benchmark = round(df_movie_summary['count'].quantile(0.7),0)
+drop_movie_list = df_movie_summary[df_movie_summary['count'] < movie_benchmark].index
+
+df_cust_summary = df.groupby('CustomerID')['Rating'].agg(f)
+df_cust_summary.index = df_cust_summary.index.map(int)
+cust_benchmark = round(df_cust_summary['count'].quantile(0.7),0)
+drop_cust_list = df_cust_summary[df_cust_summary['count'] < cust_benchmark].index
+
+df = df[~df['MovieID'].isin(drop_movie_list)]
+df = df[~df['CustomerID'].isin(drop_cust_list)]
+
+# nxm matrix (customer x movie)
+matrix = pd.pivot_table(df,values='Rating',index='CustomerID',columns='MovieID')
+
+# movieid to movie title data
+movieNames = pd.read_csv('movie_titles.csv',encoding = "ISO-8859-1", header = None, names = ['MovieID', 'Name'],usecols = [0,2])
+movieNames.set_index('MovieID', inplace=True)
+
+print (movieNames.head(10))
 
 #TODO:add col to signify if user has rated given movie 
-
 #TODO:substitute any nulls with mean 
 
 #TODO:do 5-fold cross validation
